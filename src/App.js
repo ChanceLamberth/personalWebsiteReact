1defd12fe16296058d24c7423739e7342ab2da2f
ChanceLamberth <Lamberth.c@northeastern.edu>
2022-02-17 08:57:58 -0600

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Main from './components/Main';
 import NavBar from './components/Nav';
 
 function App() {
   return (
     <div style={{backgroundColor: "white"}}>
       <NavBar/>
-      
+      <Main/>
     </div>
   );
 }
